0d66d78debf932291bc746ed215fe30652bd4fe0
Honor <mytwink21@mail.ru>
2022-07-03 12:29:35 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // Меню бургер
 const menuIcon = document.querySelector('._toggle-menu');
 const menuHeader = document.querySelector('.header');
+const body = document.querySelector('.js-body');
 if (menuIcon) {
-  	menuIcon.addEventListener("click", function (e) {
+	menuIcon.addEventListener("click", function (e) {
 		menuIcon.classList.toggle('_toggle-menu-open');
 		menuHeader.classList.toggle('header__menu-open');
+		body.classList.toggle('body--lock')
 	});
 }
